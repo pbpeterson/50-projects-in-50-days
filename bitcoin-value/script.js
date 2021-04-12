da6b408142b70b1b13e6be512e183f08961245da
Peterson Pereira <petersonbozza7@gmail.com>
2021-04-11 22:50:54 -0300

    Adicionado projeto bitcoin-value que exibe o valor do bitcoin em reais e atualiza a cada dois minutos --Javascript Assincrono

---
+++ bitcoin-value/script.js
@@ -0,0 +1,16 @@
+const content = document.querySelector('.content');
+
+async function pegaValorBtc(){
+  let valorBtc = await fetch('https://blockchain.info/ticker');
+  valorBtc = await valorBtc.json();
+  valorBtc = valorBtc.BRL.buy;
+  valorBtc = String.prototype.replace.call(valorBtc, '.', ',');
+  content.innerHTML = `<p> O Valor de 1 Bitcoin está em R$ ${valorBtc}</p>`
+};
+
+setInterval(()=>{
+  pegaValorBtc();
+}, 2000);
+
+
+
