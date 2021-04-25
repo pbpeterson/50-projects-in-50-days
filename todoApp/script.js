b903ffdda85b9cbc6d90fa1aefdc6b07a6ac7d3d
Peterson Pereira <petersonbozza7@gmail.com>
2021-04-24 21:50:26 -0300

    corrigido TodoApp erro de LocalStorage

--- todoApp/script.js
+++ todoApp/script.js
@@ -6,68 +6,67 @@ let fecharButton = document.querySelectorAll('.button');
 const limparTudo = document.querySelector('.clearAll');
 const tasks = document.querySelector('.contents');
 
-
-function setValue(){
-  tasks.innerHTML = Object.values(localStorage)
-  atualizaTask()
-  if(fecharButton.length){
+function setValue() {
+  tasks.innerHTML = window.localStorage.getItem('content');
+  atualizaTask();
+  if (fecharButton.length) {
     pendents.classList.add('ativo');
   }
-  fecharButton.forEach((fechar) =>{
-    fechar.addEventListener('click', removeSelf)
+  fecharButton.forEach((fechar) => {
+    fechar.addEventListener('click', removeSelf);
   });
 }
 
-function atualizaTask(){
+function atualizaTask() {
   fecharButton = document.querySelectorAll('.button');
   const tarefaPendente = document.querySelector('.tarefaPendente');
-  if (fecharButton.length ==1){
-    tarefaPendente.innerText = `Você tem ${fecharButton.length} tarefa pendentes`}
-  else{
-  tarefaPendente.innerText = `Você tem ${fecharButton.length} tarefas pendentes`}
+  if (fecharButton.length == 1) {
+    tarefaPendente.innerText = `Você tem ${fecharButton.length} tarefa pendentes`;
+  } else {
+    tarefaPendente.innerText = `Você tem ${fecharButton.length} tarefas pendentes`;
+  }
 }
 
-function removeSelf(event){
-    event.path[1].remove()
-    const conteudos = document.querySelectorAll('.content');
-    if(conteudos.length ==0){
-      pendents.classList.remove('ativo');
-    }
-    atualizaTask()
-    saveHtml('content', tasks.innerHTML)
+function removeSelf(event) {
+  event.path[1].remove();
+  const conteudos = document.querySelectorAll('.content');
+  if (conteudos.length == 0) {
+    pendents.classList.remove('ativo');
+  }
+  atualizaTask();
+  saveHtml('content', tasks.innerHTML);
 }
 
-function adicionarTarefa(){
+function adicionarTarefa() {
   event.preventDefault();
-  if(tarefa.value){
+  if (tarefa.value) {
     const newDiv = document.createElement('div');
-    newDiv.classList.add('content')
+    newDiv.classList.add('content');
     newDiv.innerHTML = `<p>${tarefa.value}</p> \n <p class="button">✖</p>`;
     tasks.appendChild(newDiv);
     tarefa.value = '';
     pendents.classList.add('ativo');
-    atualizaTask()
+    atualizaTask();
   }
-  fecharButton.forEach((fechar) =>{
-    fechar.addEventListener('click', removeSelf)
+  fecharButton.forEach((fechar) => {
+    fechar.addEventListener('click', removeSelf);
   });
-  saveHtml('content', tasks.innerHTML)
+  saveHtml('content', tasks.innerHTML);
 }
 
-
-function removerTudo(){
+function removerTudo() {
   const conteudos = document.querySelectorAll('.content');
-  conteudos.forEach((conteudo) =>{
+  conteudos.forEach((conteudo) => {
     conteudo.remove();
-  })
+  });
   pendents.classList.remove('ativo');
 }
 
-function saveHtml(value, html){
-  localStorage[value] = html
+function saveHtml(value, html) {
+  localStorage[value] = html;
 }
 
-setValue()
-saveHtml('content', tasks.innerHTML)
+setValue();
+saveHtml('content', tasks.innerHTML);
 adicionar.addEventListener('click', adicionarTarefa);
 limparTudo.addEventListener('click', removerTudo);
